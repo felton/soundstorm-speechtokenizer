c3ee85f2422ee1f17ac0b2b89b21bf51a6b64b4d
felton <23240+felton@users.noreply.github.com>
2024-10-25 10:27:02 -0400

    Add runpod script

---
+++ runpod.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd /workspace
+mkdir SoundStorm
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install -y vim git-lfs
+git lfs install
+git clone https://huggingface.co/fnlp/SpeechTokenizer
+wget -qO ./SoundStorm/SoundStorm_best_dev.pt --content-disposition "https://huggingface.co/fnlp/USLM/resolve/main/SoundStorm_LibriSpeech/SoundStorm_best_dev.pt?download=true"
+wget -qO- "https://www.openslr.org/resources/12/dev-clean.tar.gz" | tar -xz --no-same-owner
+git clone https://github.com/felton/soundstorm-speechtokenizer.git
+cd soundstorm-speechtokenizer
+pip install .
+git checkout runpod
+export $(cat .env | xargs)
